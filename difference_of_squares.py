74414704574229937230f490d79cc4bc9fd9c3c3
Anshika <anshikagupta45612@gmail.com>
2022-02-21 07:50:41 +0530

    difference of squares code added

---
+++ difference_of_squares.py
@@ -0,0 +1,9 @@
+
+def square_of_sum(number):
+    return sum(range(1, number+1))**2
+
+def sum_of_squares(number):
+    return sum(i*i for i in range(1, number+1))
+
+def difference_of_squares(number):
+    return square_of_sum(number)  - sum_of_squares(number)
